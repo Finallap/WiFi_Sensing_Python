23035d5a09e12e25d1d812873ed05ca2dbb2a0e8
Finallap <547805712@qq.com>
2019-09-11 08:28:51 +0800

    修改了log地址的方式，加入了Tensor Borad

--- wifi_mat.py
+++ wifi_mat.py
@@ -3,12 +3,14 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from keras.layers import Dense, LSTM, Masking, Bidirectional, Dropout, TimeDistributed
 from keras.models import Sequential
-from keras.callbacks import LearningRateScheduler, ModelCheckpoint
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint, TensorBoard, CSVLogger
+from keras.utils import plot_model
 from keras.optimizers import Optimizer
 from keras_contrib.layers.crf import CRF
 import matplotlib.pyplot as plt
 import keras.backend as K
 import numpy as np
+import keras
 
 def scheduler(epoch):
     # 每隔8个epoch，学习率减小为原来的0.8
@@ -22,6 +24,7 @@ def scheduler(epoch):
 input_feature = 180  #特征个数
 num_class = 6
 dropout_rate = 0.2
+log_dir = 'F:\\Git repository\\Experimental result\\2019_09_10_keras\\test1\\'
 
 #载入mat数据
 mat_data = loadmat("G:/无源感知研究/数据采集/2019_07_18/实验室（滤波后）.mat")
@@ -52,14 +55,14 @@ lb = LabelBinarizer()
 csi_train_label = lb.fit_transform(csi_train_label)
 
 #划分训练集和测试集
-train,test,train_label,test_label = train_test_split(csi_train_data,csi_train_label,test_size=0.3)
+train,test,train_label,test_label = train_test_split(csi_train_data,csi_train_label,test_size=0.25)
 
 #build model
 model = Sequential()
 model.add(Masking(mask_value= 0,input_shape=(sequence_max_len, input_feature)))
-model.add(Bidirectional(LSTM(128,return_sequences=True)))
+model.add(Bidirectional(LSTM(150,return_sequences=True)))
 model.add(Dropout(dropout_rate))
-model.add(Bidirectional(LSTM(128)))
+model.add(Bidirectional(LSTM(150)))
 model.add(Dropout(dropout_rate))
 #model.add(TimeDistributed(Dense(num_class)))
 #crf_layer = CRF(num_class, sparse_target=True)
@@ -68,16 +71,28 @@ model.add(Dense(num_class, activation='softmax'))
 
 #compile:loss, optimizer, metrics
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+#plot_model(model,to_file= log_dir + 'model.png')
 model.summary()
 #model.compile('rmsprop', loss=crf_layer.loss_function, metrics=[crf_layer.accuracy])
 
-#train: epcoch, batch_size
+#callbacks
 reduce_lr = LearningRateScheduler(scheduler)
-filepath = 'model-ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5'
+filepath =  log_dir + 'model-ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5'
 checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+csv_logger = CSVLogger(log_dir + 'training.log')
+tb = TensorBoard(log_dir=log_dir,  # log 目录
+                 histogram_freq=1,  # 按照何等频率（epoch）来计算直方图，0为不计算
+                 batch_size=32,     # 用多大量的数据计算直方图
+                 write_graph=True,  # 是否存储网络结构图
+                 write_grads=False, # 是否可视化梯度直方图
+                 write_images=False,# 是否可视化参数
+                 embeddings_freq=0,
+                 embeddings_layer_names=None,
+                 embeddings_metadata=None)
+
 # fit model
-history = model.fit(train, train_label, epochs=50, batch_size=64, verbose=1,
-                    callbacks=[checkpoint, reduce_lr], validation_data=(test, test_label))
+history = model.fit(train, train_label, epochs=200, batch_size=64, verbose=1,
+                    callbacks=[checkpoint, reduce_lr, tb, csv_logger], validation_data=(test, test_label))
 
 score = model.evaluate(test, test_label,batch_size=64, verbose=1)
 print(score)
@@ -90,7 +105,7 @@ plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
-plt.savefig(r'acc.png', dpi=900)
+plt.savefig( log_dir + 'acc.png', dpi=900)
 
 # 绘制训练 & 验证的损失值
 plt.plot(history.history['loss'])
@@ -100,4 +115,4 @@ plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
-plt.savefig(r'loss.png', dpi=900)
+plt.savefig( log_dir + 'loss.png', dpi=900)
