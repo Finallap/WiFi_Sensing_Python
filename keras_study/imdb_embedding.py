b3ed4cdb96ec967eb2d19b912a1456fe39a01abf
Finallap <547805712@qq.com>
2020-01-02 10:15:33 +0800

    使用embedding训练imdb

---
+++ keras_study/imdb_embedding.py
@@ -0,0 +1,21 @@
+from keras.datasets import imdb
+from keras import preprocessing
+from keras import layers
+from keras import models
+
+max_features = 1000
+maxlen = 20
+
+(x_train, y_train), (x_test, y_test) = imdb.load_data()
+
+x_train = preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = preprocessing.sequence.pad_sequences(x_test, maxlen=maxlen)
+
+model = models.Sequential()
+model.add(layers.Embedding(max_features, 8, input_length=maxlen))
+model.add(layers.Flatten())
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile('adam', 'binary_crossentropy', ['acc'])
+print(model.summary())
+
+history = model.fit(x_train, y_train, batch_size=30, epochs=10, validation_data=(x_test, y_test))
