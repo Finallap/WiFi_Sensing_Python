4c20b5a648d56d1d72aa8a46ff586c45351ff75e
Finallap <547805712@qq.com>
2020-06-01 21:57:04 +0800

    DataPrefetcher

---
+++ data_loader/DataPrefetcher.py
@@ -0,0 +1,36 @@
+import torch
+
+
+class DataPrefetcher():
+    def __init__(self, loader, device):
+        self.loader = iter(loader)
+        self.device = device
+        self.stream = torch.cuda.Stream()
+        # With Amp, it isn't necessary to manually convert data to half.
+        # if args.fp16:
+        #     self.mean = self.mean.half()
+        #     self.std = self.std.half()
+        self.preload()
+
+    def preload(self):
+        try:
+            self.batch = next(self.loader)
+        except StopIteration:
+            self.batch = None
+            return
+        with torch.cuda.stream(self.stream):
+            for k in self.batch:
+                if k != 'meta':
+                    self.batch[k] = self.batch[k].to(device=self.device, non_blocking=True)
+
+            # With Amp, it isn't necessary to manually convert data to half.
+            # if args.fp16:
+            #     self.next_input = self.next_input.half()
+            # else:
+            #     self.next_input = self.next_input.float()
+
+    def next(self):
+        torch.cuda.current_stream().wait_stream(self.stream)
+        batch = self.batch
+        self.preload()
+        return batch
