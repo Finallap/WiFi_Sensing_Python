105d443c00d37f8659b018b9ff7ad99aa8aa7e4f
Finallap <547805712@qq.com>
2020-06-07 17:07:57 +0800

    多进程csv加载

--- data_loader/concurrent_study/csv_loader.py
+++ data_loader/concurrent_study/csv_loader.py
@@ -4,9 +4,11 @@ import os
 import threading
 from tqdm import tqdm
 from tqdm import trange
+from multiprocessing import Process
 
 
-class Reader(threading.Thread):
+# class Reader(threading.Thread):
+class Reader(Process):
     def __init__(self, thread_no, file_list, file_dir, training_data, start_pos, end_pos):
         super(Reader, self).__init__()
         self.thread_no = thread_no
@@ -17,8 +19,8 @@ class Reader(threading.Thread):
         self.training_data = training_data
 
     def run(self):
-        # for i in tqdm(range(self.start_pos, self.end_pos + 1), desc='Thread' + str(self.thread_no), mininterval=1000):
-        for i in range(self.start_pos, self.end_pos + 1):
+        for i in tqdm(range(self.start_pos, self.end_pos + 1), desc='Thread' + str(self.thread_no), mininterval=100):
+            # for i in range(self.start_pos, self.end_pos + 1):
             file_path = file_dir + '/' + file_list[i]
             csv_data = np.loadtxt(file_path, dtype=np.float16, delimiter=",")
             self.training_data.append(csv_data)
@@ -57,24 +59,37 @@ if __name__ == '__main__':
     file_list = os.listdir(file_dir)
     # 线程数量
     # thread_nums = [32, 24, 16, 12, 8, 4, 2, 1]
-    thread_nums = [32, 24, 2, 1]
+    # thread_nums = [32, 24, 2, 1]
     # thread_nums = [16, 12, 8, 4]
+    thread_nums = [32]
 
     for thread_num in thread_nums:
         training_data = []
         # 起始时间
         start_time = time.clock()
+        # 划分读取部分
         p = Partition(file_list, thread_num)
-        t = []
         pos = p.part()
-        # 生成线程
+        # # 生成线程
+        # t = []
+        # for i in range(thread_num):
+        #     t.append(Reader(i, file_list, file_dir, training_data, *pos[i]))
+        # # 开启线程
+        # for i in range(thread_num):
+        #     t[i].start()
+        # for i in range(thread_num):
+        #     t[i].join()
+
+        # 创建进程
+        processes = {}
         for i in range(thread_num):
-            t.append(Reader(i, file_list, file_dir, training_data, *pos[i]))
+            processes[i] = Reader(i, file_list, file_dir, training_data, *pos[i])
         # 开启线程
         for i in range(thread_num):
-            t[i].start()
+            processes[i].start()
         for i in range(thread_num):
-            t[i].join()
+            processes[i].join()
+
         # 结束时间
         end_time = time.clock()
         print("Thread_num: %d, cost time is: %f" % (thread_num, end_time - start_time))
