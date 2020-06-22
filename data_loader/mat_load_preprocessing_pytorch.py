d8f99b678ed658add4c6a397366406448d13c20f
Finallap <547805712@qq.com>
2020-06-22 23:30:52 +0800

    适配Widar3数据集

--- data_loader/mat_load_preprocessing_pytorch.py
+++ data_loader/mat_load_preprocessing_pytorch.py
@@ -1,3 +1,4 @@
+import hdf5storage
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 import numpy as np
@@ -14,7 +15,7 @@ from data_loader.DataLoaderX import DataLoaderX
 def load_data(data_path, config):
     # 载入mat数据
     mat_data = loadmat(data_path)
-    # mat_data = hdf5storage.loadmat(mat_path)
+    # mat_data = hdf5storage.loadmat(data_path)
     # mat_data = h5py.File(data_path)
 
     # 读出csi，确定样本个数
@@ -32,6 +33,7 @@ def load_data(data_path, config):
         csi_train_data = np.zeros((sample_count, config['input_feature'], config['sequence_max_len']))
         for i in range(sample_count):
             csi_train_data[i] = csi_train[i].reshape((csi_train[i].shape[1], csi_train[i].shape[0])).astype(np.float64)
+            # csi_train_data[i] = csi_train[i].astype(np.float64)
     elif (config['model_type'] == 'lstm'):  # lstm输入格式为3维
         csi_train_data = np.zeros((sample_count, config['sequence_max_len'], config['input_feature']))
         for i in range(sample_count):
@@ -40,7 +42,8 @@ def load_data(data_path, config):
     # 创建训练使用的标签数组，将动作名称标签复制进去（人物标签暂时不复制）
     csi_train_label = []
     for i in range(len(mat_data["csi_label"])):
-        csi_train_label.append(mat_data["csi_label"][i][0][0])
+        # csi_train_label.append(mat_data["csi_label"][i][0][0])
+        csi_train_label.append(mat_data["csi_label"][i][1][0][0])
     # 将训练标签数组进行one-hot编码
     # lb = LabelBinarizer()
     # csi_train_label = lb.fit_transform(csi_train_label)
