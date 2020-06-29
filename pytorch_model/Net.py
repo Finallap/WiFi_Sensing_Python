05793ca4ed95705bc7786f590d0d545518aa2465
Finallap <547805712@qq.com>
2020-06-29 14:18:18 +0800

    pytorch模型统一加上bottleneck层

--- pytorch_model/Net.py
+++ pytorch_model/Net.py
@@ -1,19 +1,22 @@
 import torch
 import torch.nn as nn
 
+def buildModel(config):
+    return Net(config)
 
 class Net(torch.nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
 
+        self.cls_fc = nn.Linear(128, config['n_class'])
         if self.config['model_type'] == 'lstm':
             # lstm
             self.bidirectional = True
             self.num_layers = 2
             self.base_network = nn.LSTM(input_size=config['input_feature'], hidden_size=config['hidden_size'],
                                         num_layers=self.num_layers, bidirectional=self.bidirectional,
-                                        dropout=0.5, batch_first=True)
+                                        dropout=0.3, batch_first=True)
             if self.bidirectional:
                 self.bottleneck = nn.Linear(config['hidden_size'] * 2, 128)
             else:
@@ -22,7 +25,8 @@ class Net(torch.nn.Module):
             # conv1d
             # 输入的数据格式是（batch_size, word_vector, sequence_length）
             self.base_network = nn.Sequential(
-                nn.Conv1d(in_channels=180, out_channels=128, kernel_size=8, padding_mode='circular'),
+                nn.Conv1d(in_channels=config['input_feature'], out_channels=128, kernel_size=8,
+                          padding_mode='circular'),
                 nn.BatchNorm1d(num_features=128),
                 nn.ReLU(),
                 nn.Conv1d(in_channels=128, out_channels=256, kernel_size=5, padding_mode='circular'),
@@ -46,20 +50,22 @@ class Net(torch.nn.Module):
                 nn.MaxPool2d(kernel_size=2, stride=2),
                 nn.ReLU(),
             )
-            self.bottleneck = nn.Linear(config['batch_size'] * 3680, 128)
+            # self.bottleneck = nn.Linear(config['batch_size'] * 3680, 128)
+            self.bottleneck = nn.Linear(config['batch_size'] * 3968, 128)
 
     def forward(self, input):
         # lstm
         if self.config['model_type'] == 'lstm':
             x, _ = self.base_network(input)
-            output = self.bottleneck(x[:, -1, :])
+            x = self.bottleneck(x[:, -1, :])
         elif self.config['model_type'] == 'conv1d':
             # conv1d
             x = self.base_network(input)
-            output = x.view(self.config['batch_size'], -1)
+            x = x.view(self.config['batch_size'], -1)
         else:
             # conv
             x = self.base_network(input)
             x = x.view(self.config['batch_size'], -1)
-            output = self.bottleneck(x)
+            x = self.bottleneck(x)
+        output = self.cls_fc(x)
         return output
